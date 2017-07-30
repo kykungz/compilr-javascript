aa81e5009628c76b90238a10709db8a7e8c734cf
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-07-30 23:52:13 +0700

    add eslint

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-'use strict';
-const ip = require('ip');
-const fs = require('fs-extra');
-const exec = require('child-process-promise').exec;
-const winston = require('winston');
+'use strict'
+
+const ip = require('ip')
+const fs = require('fs-extra')
+const exec = require('child-process-promise').exec
+const winston = require('winston')
 const bodyParser = require('body-parser')
-const express = require('express');
+const express = require('express')
 
 // Constants
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-const app = express();
+const app = express()
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json())       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-}));
+}))
 
 // root
 app.get('/', (req, res) => {
-  res.send('api ready');
-});
+  res.send('api ready')
+})
 
 app.post('/compile/', async (req, res, next) => {
   let content = req.body.content
@@ -27,7 +28,7 @@ app.post('/compile/', async (req, res, next) => {
   let dirname
   try {
     dirname = await fs.mkdtemp('./run/tmp_')
-    console.log(`created directory ${dirname}`);
+    console.log(`created directory ${dirname}`)
 
     await fs.writeFile(`${dirname}/test.js`, content)
     console.log(`created file ${dirname}/test.js`)
@@ -44,23 +45,22 @@ app.post('/compile/', async (req, res, next) => {
         output: err.stdout + err.stderr
       })
     }
-
   } catch (e) {
     next(e)
   } finally {
     await fs.remove(dirname)
-    console.log(`removed ${dirname}`);
+    console.log(`removed ${dirname}`)
   }
 })
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-  console.log(err);
+  console.log(err)
   winston.log('error', err)
   res.status(500).render('error', {
     message: err.message,
     error: err
-  });
+  })
 })
 
 app.listen(PORT, async () => {
@@ -68,8 +68,8 @@ app.listen(PORT, async () => {
     await fs.mkdir('run')
     console.log(`created run folder`)
   } catch (e) {
-    console.log('run directory already exists');
+    console.log('run directory already exists')
   } finally {
-    console.log(`Running on http://${ip.address()}:${PORT}`);
+    console.log(`Running on http://${ip.address()}:${PORT}`)
   }
-});
+})
